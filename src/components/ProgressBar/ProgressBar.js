c034322aa2e795e6a88b0306f66bd0c77072c490
eribau <erik_bauer@hotmail.com>
2024-06-28 14:39:44 +0200

    Add adaptive radius-border to progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -9,14 +9,23 @@ const SIZES = {
 	small: {
 		"--height": 8 + "px",
 		"--border-radius": 4 + "px",
+		"--bar-height": 8 + "px",
+		"--bar-vertical-offset": 0 + "px",
+		"--bar-horizontal-offset": 0 + "px",
 	},
 	medium: {
 		"--height": 12 + "px",
 		"--border-radius": 4 + "px",
+		"--bar-height": 12 + "px",
+		"--bar-vertical-offset": 0 + "px",
+		"--bar-horizontal-offset": 0 + "px",
 	},
 	large: {
 		"--height": 24 + "px",
 		"--border-radius": 8 + "px",
+		"--bar-height": 16 + "px",
+		"--bar-vertical-offset": 4 + "px",
+		"--bar-horizontal-offset": 4 + "px",
 	}
 }
 
@@ -34,15 +43,21 @@ const Wrapper = styled.div`
 	width: 370px;
 	background: ${COLORS.transparentGray15};
 	border-radius: var(--border-radius);
+	box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
 	
 	&::before {
 		content: "";
 		position: absolute;
-		top: 0;
-		left: 0;
-		height: 100%;
-		width: ${props => props.value + "%"};
+		top: var(--bar-vertical-offset);
+		left: var(--bar-horizontal-offset);
+		height: var(--bar-height);
+		width: calc(${props => props.value + "%"} - (var(--bar-vertical-offset) * 2));
 		background: ${COLORS.primary};
+		border-radius: 4px 0 0 4px;
+		border-top-left-radius: 4px;
+		border-bottom-left-radius: 4px;
+		border-top-right-radius: max(${props => props.value + "%"} + var(--border-radius) - var(--bar-horizontal-offset) - 100%, 0px);
+		border-bottom-right-radius: max(${props => props.value + "%"} + var(--border-radius) - var(--bar-horizontal-offset) - 100%, 0px);
 	}
 `;
 
