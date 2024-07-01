78e13b10b7b7f11bf07e085d31d65f62b15308d0
eribau <erik_bauer@hotmail.com>
2024-07-01 19:32:40 +0200

    Add select style

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,35 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <SelectBase value={value} onChange={onChange}>
+        {children}
+      </SelectBase> 
+      <Icon id="chevron-down"></Icon>
+    </Wrapper> 
   );
 };
+
+const Wrapper = styled.div`
+    height: 43px;
+    background-color: ${COLORS.transparentGray15};
+    border-radius: 8px;
+    width: 100%;
+    min-width: 15ch;
+    max-width: 30ch;
+
+`;
+
+const SelectBase = styled.select`
+  appearance: none;
+  background-color: transparent;
+  border: none;
+  margin: 0;
+  width: 100%;
+  font-family: inherit;
+  font-size: inherit;
+  cursor: inherit;
+  line-heigth: inherit;
+`;
 
 export default Select;
