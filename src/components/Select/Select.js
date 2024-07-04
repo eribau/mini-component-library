0c87bb6a83797c37d5ff67f38e4f420c90c04558
eribau <erik_bauer@hotmail.com>
2024-07-04 20:15:53 +0200

    Add adjusting width, focus state and hover state to select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,7 +9,7 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <Wrapper>
+    <Wrapper textLength={displayedValue.length}>
       <SelectBase value={value} onChange={onChange}>
         {children}
       </SelectBase> 
@@ -22,22 +22,40 @@ const Wrapper = styled.div`
     height: 43px;
     background-color: ${COLORS.transparentGray15};
     border-radius: 8px;
-    width: 100%;
-    min-width: 15ch;
-    max-width: 30ch;
+    width: ${props => "calc(" + props.textLength + "ch + 48px)"};
+    display: flex;
+    position: relative;
 
-`;
+    & > div {
+      font-weight: 700;
+      position: absolute;
+      top: 9.5px;
+      right: 12px;
+      z-index: -1;
+    }
+    `;
 
 const SelectBase = styled.select`
   appearance: none;
   background-color: transparent;
   border: none;
   margin: 0;
+  padding: 0px 36px 0 12px;
   width: 100%;
+  height: 100%;
   font-family: inherit;
   font-size: inherit;
+  color: ${COLORS.gray700};
   cursor: inherit;
   line-heigth: inherit;
+
+  &:focus {
+    border-radius: inherit;
+  }
+
+  &:hover {
+    color: black;
+  }
 `;
 
 export default Select;
